fe875f71c44418890b87c32fd24283139024489a
Mario Merino <mmerino1010@protonmail.com>
2019-12-29 20:58:44 -0600

    semantic ui update and okta login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import Staff from "./components/pages/Staff";
+import Login from "./components/auth/Login";
+import Pricing from "./components/pages/Pricing";
+
+import "./App.css";
+
+function onAuthRequired({ history }) {
+  history.push("./login");
 }
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security
+          issuer="https://dev-928238.okta.com/oauth2/default"
+          clientId="0oa28ek59bNPHO7Ct357"
+          redirectUri={window.location.origin + "/implicit/callback"}
+          onAuthRequired={onAuthRequired}
+          pkce={true}
+        >
+          <div className="App">
+            <NavBar />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/pricing" exact={true} component={Pricing} />
+            <SecureRoute path="/staff" exact={true} component={Staff} />
+            <Route
+              path="/login"
+              render={() => <Login baseUrl="https://dev-928238.okta.com" />}
+            />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+          </div>
+        </Security>
+      </Router>
+    );
+  }
+}
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+export default class MenuExampleStackable extends Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu>
+        <Menu.Item
+          as={Link}
+          active={activeItem === "Home"}
+          onClick={this.handleItemClick}
+          link={true}
+          to="/"
+        >
+          <h1> logo</h1>
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/pricing"
+          name="Pricing"
+          active={activeItem === "Pricing"}
+          onClick={this.handleItemClick}
+        >
+          News
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/pricing"
+          name="Pricing"
+          active={activeItem === "Pricing"}
+          onClick={this.handleItemClick}
+        >
+          Pricing
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item
+            as={Link}
+            to="/login"
+            name="sign-in"
+            active={activeItem === "Login"}
+            onClick={this.handleItemClick}
+          ></Menu.Item>
+          <Menu.Item
+            as={Link}
+            name="staff"
+            active={activeItem === "staff"}
+            onClick={this.handleItemClick}
+            link={true}
+            to="/staff"
+          >
+            Staff
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
